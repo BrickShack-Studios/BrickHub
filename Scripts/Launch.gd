13f0b51c9cc2817fb6061601f8e2ba095c5f0a42
Frank Rodriguez <frank.rodriguez1337@gmail.com>
2018-06-04 06:21:56 -0400

    Logging and main menu auto-updates

--- Scripts/Launch.gd
+++ Scripts/Launch.gd
@@ -1,15 +1,16 @@
 extends Object
 
-var parser = XMLParser.new()
+var logger = load("res://Scripts/Logger.gd").new()
 var linksFile = ""
 
 onready var Hasher = load("res://Scripts/Hasher.gd").new()
 
 func parseJSON(path):
+	logger.logLine("Parsing " + path + "...")
 	var file = File.new()
 	
 	if (!file.file_exists(path)):
-		print("Error: " + path + " does not exist!")
+		logger.logLine("Error: " + path + " does not exist!")
 	else:
 		file.open(path, File.READ)
 		var contents = file.get_as_text()
@@ -18,22 +19,25 @@ func parseJSON(path):
 		var parsed = JSON.parse(contents)
 		
 		if (parsed.error != 0):
-			print("Error parsing " + path + "! Details:")
-			print(parsed.error)
-			print(parsed.error_line)
-			print(parsed.error_string)
+			logger.logLine("Error parsing " + path + "! Details:")
+			logger.logLine(parsed.error)
+			logger.logLine(parsed.error_line)
+			logger.logLine(parsed.error_string)
 		else:
+			logger.logLine("Successfully parsed " + path)
 			return parsed.result
 	
 	return null
 
 func _ready():
+	logger.logLine("Attempting to download database...")
 	var file = File.new()
 	var jsonPath = "res://Defaults/database.json"
 	
 	if (file.file_exists("user://database.json")):
-		print("Database exists")
 		jsonPath = "user://database.json"
+	else:
+		logger.logLine("Fresh install detected")
 	
 	var json = parseJSON(jsonPath)
 	$InitialUpdater.start(json.brickHub.databaseLink, "user://database.tmp")
@@ -49,8 +53,9 @@ func start():
 	return
 
 func _on_InitialUpdater_finishedUpdate(response):
+	logger.logLine("Updating database...")
 	if (response != 0):
-		print("Error grabbing initial update. While the old caches will work, note that BrickHub may be out of date. Check your internet connection.")
+		logger.logLine("Error grabbing initial update. While the old caches will work, note that BrickHub may be out of date. Check your internet connection.")
 		var file = File.new()
 		if (!file.file_exists("user://database.json")):
 			var dir = Directory.new()
@@ -60,6 +65,7 @@ func _on_InitialUpdater_finishedUpdate(response):
 		var file = File.new()
 		file.open("user://database.tmp", File.READ)
 		if (file.get_len() > 0):
+			logger.logLine("Database downloaded successfully")
 			file.close()
 			
 			var dir = Directory.new()
@@ -70,15 +76,18 @@ func _on_InitialUpdater_finishedUpdate(response):
 		else:
 			file.close()
 	
-	print("Updated database")
+	logger.logLine("Updated database")
 	
 	loadTabs()
 	return
 	
 func loadTabs():
+	logger.logLine("Initializing tabs...")
 	var database = parseJSON("user://database.json")
 	
-	print("Initializing tabs...")
+	$TabContainer/BrickHub.initialize(	database.brickHub.description,
+										database.brickHub.news,
+										"")
 	for element in database.games:
 		var scene = load("res://Objects/GameTab.tscn")
 		var sceneInstance = scene.instance()
@@ -87,6 +96,6 @@ func loadTabs():
 									element.news,
 									element.downloadLink)
 		$TabContainer.add_child(sceneInstance)
-	print("Tabs initialized")
+	logger.logLine("Tabs initialized")
 	
 	return
---
+++ Scripts/Logger.gd
@@ -0,0 +1,30 @@
+extends Node
+
+func _init():
+	var file = File.new()
+	if (!file.file_exists("user://log.txt")):
+		file.open("user://log.txt", File.WRITE)
+		file.close()
+	
+	return
+
+func getDateTimeString():
+	#Returns current datetime as a dictionary of keys:
+	#	 year, month, day, weekday, dst (daylight savings time), 
+	#	hour, minute, second.
+	var dt = OS.get_datetime()
+	return str(dt.year) + "-" + str(dt.month) + "-" + str(dt.day) + " " + str(dt.hour) + ":" + str(dt.minute) + ":" + str(dt.second)
+
+func logLine(line, silent = false):
+	if (!silent):
+		print(line)
+	
+	var file = File.new()
+	file.open("user://log.txt", File.READ_WRITE)
+	
+	file.seek_end()
+	file.store_string(getDateTimeString() + "\t\t" + line + "\n")
+	
+	file.close()
+	
+	return
